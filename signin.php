f4ed451adcf4f582ecc92d5c062adce002352193
Kelvin Kaburu <kelvinmwenda9@gmail.com>
2019-09-24 16:19:32 +0300

    Code Review
    Remove html code

--- signin.php
+++ signin.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <title>USER LOGIN</title>
-</head>
-<body>
-  <h1 class="jumbotron alert alert-info">Safyrisha Login Form</h1>
-  <p>(Safyrisha) Please Fill Your User Credentials.</p>
-  <!-- user_id(Auto),email(Unique), username, password, confirm, role, saved_date(timestamp) (You don't save password and confirm in the table)-->
-  <!-- create table tbl_users -->
-  <!-- create a form and save the record -->
-  <form method="POST">
-    <input type="email" name="email" placeholder="enter your email"><br><br>
-    <input type="password" name="password" placeholder="enter your password"><br><br>
-    <input type="submit" name="submit">
-
-  </form><br>
-
-  <p>Not Registered?  <a href="register.php">Register Here</a></p>
-  <!-- create login.php and a form with email and password -->
-
-</body>
-</html>
 
 <?php 
   
