d53ea23e5b7531a72a99bcea03df9a71d23ff352
Udeme Chris <udemechris1@gmail.com>
2019-09-25 17:14:06 +0100

    Modified time field's datatype in database to timestamp which will insert the current datetime value by default, removed phonenumber,merged first and lastname to fullname, added a foreign key to make sure userId in expense table exists in users table, created a dashboard API

--- route.php
+++ route.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-**Design /UI/UX: 
-**Frontend:
-**Backend:niyiojeyinka,
+ **Design /UI/UX:
+ **Frontend:
+ **Backend:niyiojeyinka,UD_CHRIS
 
-*/
+ */
 
-$url_array =explode("/", $_SERVER['REQUEST_URI']);
+$url_array = explode("/", $_SERVER['REQUEST_URI']);
 //am getting the link details here and i split with "/"
 
 $indexOfIndexPHP = array_search("api.php", $url_array);
@@ -15,31 +15,34 @@ $indexOfIndexPHP = array_search("api.php", $url_array);
 
 //routing
 
-if (array_key_exists($indexOfIndexPHP+1, $url_array) &&  $url_array[$indexOfIndexPHP+1] !="") {
-	//if url as first parameter and the parameter is not /
+if (array_key_exists($indexOfIndexPHP + 1, $url_array) && $url_array[$indexOfIndexPHP + 1] != "") {
+    //if url as first parameter and the parameter is not /
 
-	switch ( $url_array[$indexOfIndexPHP+1]) {
-		   /* case 'register':
-			require_once "register.php";
-			break;*/
-			case 'login':
-			require_once "login.php";
-			case 'dashboard':
-			require_once "dashboard.php";
-			break;
-			default:
-			echo "404";
+    switch ($url_array[$indexOfIndexPHP + 1]) {
+        /* case 'register':
+        require_once "register.php";
+        break;*/
+        case 'login':
+            require_once "login.php";
+        case 'dashboard':
+            require_once "dashboard.php";
+            echo $dashboard->totalExpenses($id); /*Where id is user's id in database. Can be fetched and held in a session variable during login
+            Expected Output:
+            {
+            "id":    1
+            "day":    "4000.00"
+            "month":    "4000.00"
+            "year":    "4000.00"
+            }
+             */
+            break;
+        default:
+            echo "404";
 
-	}
+    }
 
-}else{
-	//home
+} else {
+    //home
 
-	echo "This Home, Direct Access Not Allowed";
+    echo "This Home, Direct Access Not Allowed";
 }
-
-
-
-
-
- ?>
