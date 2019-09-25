a2c86cc80828aeab652631b36dee2429214f0e47
pryme0 <obochi2@gmail.com>
2019-09-25 22:04:47 +0100

    added logic for update query in database

--- backend/Database.php
+++ backend/Database.php
@@ -1,56 +1,53 @@
 <?php
 /**
  * @Description:this handle the database connection
-and server as mini query builder
+  and server as mini query builder 
  */
-class Database
-{
-    public $conn;
-    public function __construct()
-    {
 
-        $this->conn = new mysqli("localhost", "root", "", "tracker");
+  //DB name Tracker
+class Database{
+	public $conn;
+	function __construct()
+	{
+    
+  $this->conn = new mysqli("localhost", "root", "","tracker");
 // Check if connected
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
+if ($this->conn->connect_error) {
+    die("Connection failed: " . $this->conn->connect_error);
+}
 
-    }
-    public function query($sql)
-    {
-        if ($this->conn->query($sql) === true) {
-            return true;
-        }
-        return false;
-    }
-    public function select($sql)
-    {
-        $result = $this->conn->query($sql);
+	}
+	public function query($sql)
+	{
+		if ($this->conn->query($sql) === TRUE) {
+         return TRUE;
+		}
+		return FALSE;
+	}
+	public function select($sql)
+	{
+	 $result = $this->conn->query($sql);
 
-        if ($result->num_rows > 0) {
-            $resultToReturn = [];
-            while ($row = $result->fetch_assoc()) {
-                array_push($resultToReturn, $row);
-            }
-            return $resultToReturn;
-        }
-        return 0;
-    }
-    public function selectDashboardExpenses($sql)
-    {
-        $result = $this->conn->query($sql);
-        if ($result->num_rows > 0) {
-            $resultToReturn = [];
-            while ($row = $result->fetch_assoc()) {
-                array_push($resultToReturn, $row);
-            }
-            return $resultToReturn[0]['expenses'];
-        }
-        return 0;
-    }
+		if ($result->num_rows > 0) {
+          $resultToReturn = [];
+           while($row =$result->fetch_assoc()){
+           	array_push($resultToReturn, $row);
+           }
+		return $resultToReturn;
+	}
+	return 0;
+}
 
-    public function close()
-    {
-        $this->conn->close();
-    }
+public function update($sql)
+	{
+		if ($this->conn->query($sql) === TRUE) {
+         return TRUE;
+		}
+		return FALSE;
+	}
+
+	public function close()
+	{
+		$this->conn->close();
+	}
 }
