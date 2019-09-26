8976ab164716ab46e11b04b9f21d6e3deea94143
Akinlade Feranmi <30551780+Stacalpha@users.noreply.github.com>
2019-09-26 13:06:49 +0100

    New folder structure. (#46)
    * added addExpense.php
    
    This scripts will enable the user to add a new expense.
    
    * I implemented the logout functionalities. (Backend)
    
    * change the param in include()
    
    change from install.php to Database.php
    
    * added the deleteExpense Endpoint and function
    
    * problem - backend still pushing to repo root...
    
    * deleted the addExpense.php and update the contributing file
    
    * removed duplicate addExpense function and left the one that return json
    
    * set the json returned on successfull login to include both error status and user object
    
    * fixed the semi colon bug on line 39 in signin.php
    
    * Folder structure changed for heroku deployment. (#44)
    
    * Revert "Backend register and signin"
    
    * v0.0.0 (#41)
    
    * Initial commit
    
    * starting to create the database preparation file
    
    * added route file
    
    * added install file to auto prepare for the app
    
    * added the query helper class
    
    * added the test file that test the query helper class
    
    * deleted the databaseprep.php file
    
    * added example of using the select method to select from db
    
    * made the select methof in database.php to return multi array containing each records selected
    
    * Add files via upload
    
    * added readme file for contributing doc
    
    * added login endpoint
    
    * added login to the route
    
    * Updated CONTRIBUTING GUIDELINES.
    
    Input from @Niyiojeyinka .
    
    * Added contributing guidelines for backend.
    
    Incorporated the contribution/suggestion from @niyiojeyinka into the CONTRIBUTING.md
    
    Thanks Niyi.
    
    * added addExpense.php
    
    This scripts will enable the user to add a new expense.
    
    * I implemented the logout functionalities. (Backend)
    
    * removed the phone field from db schema in install.php
    
    * removed the firstname and lastname field from db schema in install.php
    
    * added the fullname field
    
    * added contributing and deleted readme file
    
    * deleted the route file
    
    * Added README file
    
    * change the param in include()
    
    change from install.php to Database.php
    
    * Create dashboard.php
    
    * Modified time field's datatype in database to timestamp which will insert the current datetime value by default, removed phonenumber,merged first and lastname to fullname, added a foreign key to make sure userId in expense table exists in users table, created a dashboard API
    
    * Just a little correction on dashboard.php and route.php
    
    * Landing Page
    
    * deleted the login.php file and edited the signin endpoint to return json data
    
    * returned data response in json format
    
    * returned data response in json format
    
    * added a dashboard route to call a function on demand based on the url
    
    * changed the table name to correct one
    
    * set default value for the parameter variable
    
    * add the addexpense endpoint to the dashboard class
    
    * added the deleteExpense Endpoint and function
    
    * Improved login page
    
    * moved index.html to root, dashboard.php into backend
    
    * added curve background
    
    * problem - backend still pushing to repo root...
    
    * added curved background header to login page
    
    * deleted the addExpense.php and update the contributing file
    
    * removed duplicate addExpense function and left the one that return json
    
    * set the json returned on successfull login to include both error status and user object
    
    * fixed the semi colon bug on line 39 in signin.php
    
    * added config files for deployment
    
    * further updates to folder structure for deployment reasons

---
+++ web/backend/Database.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @Description:this handle the database connection
+and server as mini query builder
+ */
+class Database
+{
+    public $conn;
+    public function __construct()
+    {
+
+        $this->conn = new mysqli("localhost", "root", "", "tracker");
+// Check if connected
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+
+    }
+    public function query($sql)
+    {
+        if ($this->conn->query($sql) === true) {
+            return true;
+        }
+        return false;
+    }
+    public function select($sql)
+    {
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $resultToReturn = [];
+            while ($row = $result->fetch_assoc()) {
+                array_push($resultToReturn, $row);
+            }
+            return $resultToReturn;
+        }
+        return 0;
+    }
+    public function selectDashboardExpenses($sql)
+    {
+        $result = $this->conn->query($sql);
+        if ($result->num_rows > 0) {
+            $resultToReturn = [];
+            while ($row = $result->fetch_assoc()) {
+                array_push($resultToReturn, $row);
+            }
+            return $resultToReturn[0]['expenses'];
+        }
+        return 0;
+    }
+
+    public function close()
+    {
+        $this->conn->close();
+    }
+}
