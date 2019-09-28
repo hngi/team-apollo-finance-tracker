955192b4a27bd09a190dcb795fd3a61e814709fd
OJEYINKA OLANIYI PHILIP <Olaniyiojeyinka@gmail.com>
2019-09-28 10:18:57 +0100

    modified accordingly

--- web/backend/login.php
+++ web/backend/login.php
@@ -1,43 +1,59 @@
-<?php
-session_start();
+<?php 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Method: *");
 header('Content-Type: application/json');
+  
+if (empty($_POST)) {
+	$data = array(
+		"error" => 1,
+		"errorMessage" => "Field Cannot Be Empty",
+		"report"=> "emptyFields"
+  );
+	echo json_encode($data, true);
+}
 
 $email = $_POST['email'];
-$password = $_POST['password'];
+$password=  md5($_POST["password"]);
 
-require_once "Database.php";
-$db = new Database();
-
-//selecting from db
-$users = $db->select("SELECT * FROM users WHERE email ='".$email."';");
-if ($users == 0) {
-	$data = array (
-		"error"=>1,
-		"errorMessage" => "Either Email is Incorrect or Account Not Exists",
-		"report" =>"accountNotExists"
-	); 
-	echo json_encode($data,true);
-}
 else {
-	if ($users[0]['password'] == $password) {
-		echo json_encode($users[0], true);
-	}
-	else {
-		$data = array (
+	require_once "Database.php";
+	$db = new Database();
+
+	//selecting from db
+	$users = $db->select("SELECT * FROM users WHERE email ='".$email."';");
+	if ($users == 0) {
+		$data = array(
 			"error"=>1,
-			"errorMessage" => "Incorrect Password",
-			"report"=>"incorrectLoginDetails"
-		);
-		echo json_encode($data, true);
+			"errorMessage" => "Either Email is Incorrect or Account Not Exists",
+			"report" =>"accountNotExists"
+		); 
+		echo json_encode($data,true);
 	}
-}
+
+	else {
+  	if ($users[0]['password'] == $password) {
+    session_start();
+      $_SESSION['userId'] = $users[0]['id'];
+      $data = array(
+				"error"=>0,
+				"successMessage" => "Login Successfull",
+				"user" =>$users[0],
+				"report"=>"loggedIn"
+			);
+    	echo json_encode($data,true);
+		}
+
+		else {
+			$data = array(
+				"error"=>1,
+				"errorMessage" => "Incorrect Password",
+				"report"=>"incorrectLoginDetails"
+			);
+			echo json_encode($data,true);
+  	}
+	}
 
 $db->close();
+}
 ?>
-
-
-
-
--- web/backend/redundant.signin.php
+++ web/backend/redundant.signin.php
@@ -1,59 +1,43 @@
-<?php 
+<?php
+session_start();
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Method: *");
 header('Content-Type: application/json');
-  
-if (empty($_POST)) {
-	$data = array(
-		"error" => 1,
-		"errorMessage" => "Field Cannot Be Empty",
-		"report"=> "emptyFields"
-  );
-	echo json_encode($data, true);
-}
 
 $email = $_POST['email'];
-$password=  md5($_POST["password"]);
+$password = $_POST['password'];
 
+require_once "Database.php";
+$db = new Database();
+
+//selecting from db
+$users = $db->select("SELECT * FROM users WHERE email ='".$email."';");
+if ($users == 0) {
+	$data = array (
+		"error"=>1,
+		"errorMessage" => "Either Email is Incorrect or Account Not Exists",
+		"report" =>"accountNotExists"
+	); 
+	echo json_encode($data,true);
+}
 else {
-	require_once "Database.php";
-	$db = new Database();
-
-	//selecting from db
-	$users = $db->select("SELECT * FROM users WHERE email ='".$email."';");
-	if ($users == 0) {
-		$data = array(
-			"error"=>1,
-			"errorMessage" => "Either Email is Incorrect or Account Not Exists",
-			"report" =>"accountNotExists"
-		); 
-		echo json_encode($data,true);
+	if ($users[0]['password'] == $password) {
+		echo json_encode($users[0], true);
 	}
-
 	else {
-  	if ($users[0]['password'] == $password) {
-    session_start();
-      $_SESSION['userId'] = $users[0]['id'];
-      $data = array(
-				"error"=>0,
-				"successMessage" => "Login Successfull",
-				"user" =>$users[0],
-				"report"=>"loggedIn"
-			);
-    	echo json_encode($data,true);
-		}
-
-		else {
-			$data = array(
-				"error"=>1,
-				"errorMessage" => "Incorrect Password",
-				"report"=>"incorrectLoginDetails"
-			);
-			echo json_encode($data,true);
-  	}
+		$data = array (
+			"error"=>1,
+			"errorMessage" => "Incorrect Password",
+			"report"=>"incorrectLoginDetails"
+		);
+		echo json_encode($data, true);
 	}
+}
 
 $db->close();
-}
 ?>
+
+
+
+
