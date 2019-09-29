76c1ec699e7b481aaad343c295dc46e5fa464858
OJEYINKA OLANIYI PHILIP <Olaniyiojeyinka@gmail.com>
2019-09-29 08:35:53 +0100

    added logout func

--- web/backend/logout.php
+++ web/backend/logout.php
@@ -2,27 +2,18 @@
 session_start();
 session_unset();
 session_destroy();
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Method: *");
-header('Content-Type: application/json');
+ header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Method: *");
+  header('Content-Type: application/json');
 
 $data = array(
-	"error"=>0,
-	"successMessage" => "User Logged Out Successfully",
-	"report"=>"loggedOut"
-);
+    "error"=>0,
+    "successMessage" => "User Logged Out Successfully",
+    "report"=>"loggedOut"
+  );
 
-echo json_encode($data,true);
 
-function log_out_user() {
-	unset($_SESSION['user_id']);
-	unset($_SESSION['last_login']);
-	unset($_SESSION['email']);
-	return true;
-}
+  echo json_encode($data,true);
 
-log_out_user();
-
-redirect_to(url_for('login.php'));
 ?>
