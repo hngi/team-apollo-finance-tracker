d7379a69396bd6b5f4dda3c8ddaf3028bfc48aae
Bukkyolomolehin <54312885+Bukkyolomolehin@users.noreply.github.com>
2019-10-03 20:29:13 +0100

    Fixed validation bug in login page.

---
+++ login.php
@@ -0,0 +1,71 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Method: *");
+header('Content-Type: application/json');
+  
+if (empty($_POST)) {
+	$data = array(
+		"error" => 1,
+		"errorMessage" => "Field Cannot Be Empty",
+		"report"=> "emptyFields"
+  );
+	echo json_encode($data, true);
+}
+else{
+	$email = $_POST['email'];
+    $password=  md5($_POST["password"]);
+
+	require_once "Database.php";
+	$db = new Database();
+
+	//selecting from db
+	$users = $db->select("SELECT * FROM users WHERE email ='".$email."';");
+
+
+	//Remove all illegal characters from email
+
+	if(filter_var($email, FILTER_SANITIZE_EMAIL)){
+		$data = array(
+		"error"=>1,
+		"errorMessage" => "You have entered an invalid Email ",
+		"report" =>"emailInvalid"
+		); 
+	    echo json_encode($data,true);
+
+	}
+	if ($users == 0) {
+		$data = array(
+			"error"=>1,
+			"errorMessage" => "Either Email is Incorrect or Account Not Exists",
+			"report" =>"accountNotExists"
+		); 
+		echo json_encode($data,true);
+	}
+
+	else {
+  	if ($users[0]['password'] == $password) {
+    session_start();
+      $_SESSION['userId'] = $users[0]['id'];
+      $data = array(
+				"error"=>0,
+				"successMessage" => "Login Successfull",
+				"user" =>$users[0],
+				"report"=>"loggedIn"
+			);
+    	echo json_encode($data,true);
+		}
+
+		else {
+			$data = array(
+				"error"=>1,
+				"errorMessage" => "Incorrect Password",
+				"report"=>"incorrectLoginDetails"
+			);
+			echo json_encode($data,true);
+  	}
+	}
+
+$db->close();
+}
+?>
